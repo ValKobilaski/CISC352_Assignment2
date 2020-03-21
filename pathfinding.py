27e0142342a7ddc0737bded9c3c2533ddd6bb0b2
Sam McPhail <sam.mcphail9@gmail.com>
2020-03-21 16:33:58 -0400

    Readability improvements

--- pathfinding.py
+++ pathfinding.py
@@ -5,17 +5,13 @@ def read_grid(file_name):
 	inputGrid = []
 	inputGrid.append([])
 	with open(file_name, 'r') as inputF:
-		boardCount=0
+		boardCount = 0
 		for line in inputF.read().splitlines():
 			inputGrid[boardCount].append(line)
 			if line == "":
-				boardCount+=1
+				boardCount += 1
 				inputGrid.append([])
 	return inputGrid
-
-
-def Greedy_Search(grid, diagonal):
-	return A_Star(grid,diagonal,True)
 
 
 def get_neighbors(graph, currentX, currentY, diagonals=False):
@@ -57,17 +53,16 @@ def A_Star(grid, diagonal, greedy=False):
 	for row in range(len(grid)):
 		for col in range(len(grid[row])):
 			if grid[row][col] == 'S':
-				startX = row
-				startY = col
+				start = (row, col)
 
 	for row in range(len(grid)):
 		for col in range(len(grid[row])):
 			if grid[row][col] == 'G':
-				goalX = row
-				goalY = col
-
-	start_node = ((chebyshev((startX, startY), (goalX, goalY)) if diagonal else manhattan(
-		(startX, startY), (goalX, goalY))), startX, startY)
+				goal = (row, col)
+	
+	priority = heuristic(start, goal, diagonal)
+	start_node = (priority, start[0], start[1])
+	
 	came_from[start_node] = None
 	cost_so_far[(start_node[1], start_node[2])] = 0
 
@@ -80,26 +75,41 @@ def A_Star(grid, diagonal, greedy=False):
 		#Node is x and y without priority
 		for node in get_neighbors(grid, current[1], current[2], diagonal):
 			#If it's greedy this will just always be 0
-			new_cost = cost_so_far[current[1], current[2]] + 1 if not greedy else 0
+			new_cost = 0
+			if not greedy:
+				new_cost += cost_so_far[current[1], current[2]] + 1
+
 			if not node in cost_so_far:
-				priority = new_cost + (chebyshev(node, (goalX, goalY)) if diagonal else (manhattan(node, (goalX, goalY))))
+				priority = new_cost + heuristic(node, goal, diagonal)
 				cost_so_far[(node[0], node[1])] = new_cost
 				heapq.heappush(q, (priority, node[0], node[1]))
 				came_from[(priority, node[0], node[1])] = current
 
 	temp = came_from[current]
 	route = [temp]
-	while(came_from[temp] is not None):
+	while came_from[temp] is not None:
 		route.append(came_from[temp])
-		grid[temp[1]] = grid[temp[1]][:temp[2]] + \
-			'P' + grid[temp[1]][temp[2]+1:]
+		grid[temp[1]] = grid[temp[1]][:temp[2]] + 'P' + grid[temp[1]][temp[2]+1:]
 		temp = came_from[temp]
+
 	grid[route[-1][1]] = grid[route[-1][1]][:route[-1][2]] + 'S' + grid[route[-1][1]][route[-1][2]+1:]
 	return grid
 
+
+def Greedy_Search(grid, diagonal):
+	return A_Star(grid,diagonal,True)
+
+
+def heuristic(current, goal, diagonal):
+	if diagonal:
+		return chebyshev(current, goal)
+	else:
+		return manhattan(current, goal)
+
+
 # Takes two points (x1, y1), (x2, y2)
 def manhattan(point_a, point_b):
-	return abs(point_a[0] - point_b[0]) + abs(point_a[1] - point_a[1])
+	return abs(point_a[0] - point_b[0]) + abs(point_a[1] - point_b[1])
 
 
 # Takes two points (x1, y1), (x2, y2)
@@ -113,9 +123,10 @@ def main():
 		for grid in grids_a:
 			#If statements just to make the printing more legible
 			out.write("\nGreedy\n" if grids_a[0] != grid else 'Greedy\n')
-			out.writelines(map((lambda x: "\n" + x), Greedy_Search(grid, False)))
+			out.writelines(map((lambda x: x + "\n"), Greedy_Search(grid, False)))
 			out.write("\nA*\n" if grids_a[0] != grid else 'A*\n')
-			out.writelines(map((lambda x: "\n" + x), A_Star(grid, False)))
+			out.writelines(map((lambda x: x + "\n"), A_Star(grid, False)))
+	
 	grids_b = read_grid('pathfinding_b.txt')
 	with open('pathfinding_b_out.txt', 'w+') as out:
 		for grid in grids_b:
